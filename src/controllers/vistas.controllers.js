1c73dd299520bb14bdeb99b66dc7bb3eecd25d66
Carlos Arancibia <carlos.arancibia.a@protonmail.com>
2024-04-18 03:59:30 -0300

    Implementación de autocompletado de la vista /actualizar

--- src/controllers/vistas.controllers.js
+++ src/controllers/vistas.controllers.js
@@ -1,10 +1,16 @@
+import Handlebars from 'handlebars'
+
 const mostrarIndex = async (req, res) => {
     try {
         const response = await fetch('http://localhost:3000/api/v1/clientes');
         const data = await response.json();
         let datos = data.datos
-        console.log(datos)
-        res.render('index', { datos })
+        // Acá creo un helper para utilizarlo en la plantilla de mi index.hbs y poder mandar el objeto con el cual pintamos los datos en la fila por un input y poder utilizar esos datos para rellenar por defecto los input del formulario 'Actualizar cliente' y la edición de los daros sea más fácil. El helper hace posible usar el método JSON.stringify en la plantilla misma y de esta manera podemos convertir un objeto recibido en nuestra desde una iteración de una arreglo en una cadena de texto JSON para poder enviarla nuevamente en, por ej, un input como hicimos en este caso
+        Handlebars.registerHelper('json', function(context) {
+            return JSON.stringify(context);
+        })
+
+        res.render('index', {datos})
     } catch (err) {
         console.log(err)
     }
@@ -21,9 +27,9 @@ const mostrarFormCrear = (req, res) => {
 
 const mostrarFormActualizar = async (req, res) =>{
     try {
-      let  {id} = req.body;
-        console.log(req.body)
-        res.render('formularioActualizar', {id})
+      let  datos = JSON.parse(req.body.datos);
+
+        res.render('formularioActualizar', datos)
     } catch (err) {
         console.log(err)
     }
--- src/routes/vistas.routes.js
+++ src/routes/vistas.routes.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get('/', mostrarIndex)
 router.get('/crear', mostrarFormCrear)
-router.get('/actualizar', mostrarFormActualizar)
+router.post('/actualizar', mostrarFormActualizar)
 
 
 
